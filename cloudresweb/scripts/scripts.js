ce35ec1c48e34e35913f70a7c9ffcd3fe6dec7ef
Eric Swantek <162048271+emswantek@users.noreply.github.com>
2024-07-07 15:04:19 -0400

    Added commented out page load function

--- cloudresweb/scripts/scripts.js
+++ cloudresweb/scripts/scripts.js
@@ -2,3 +2,11 @@ function toggledark() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+// Commented out... may not need this with Azure Function
+/* document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      initApplication();
+    }
+  };
+  */ 
