00a5e3f2a3aeb4a88da9cc17a408fa87db7029d4
Eric Swantek <162048271+emswantek@users.noreply.github.com>
2024-07-07 16:23:39 -0400

    JS readystate test

--- cloudresweb/scripts/scripts.js
+++ cloudresweb/scripts/scripts.js
@@ -3,10 +3,6 @@ function toggledark() {
     element.classList.toggle("dark-mode");
 }
 
-// Commented out... may not need this with Azure Function
-/* document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
-      initApplication();
-    }
-  };
-  */ 
+function pagestate() {
+ document.getElementById("pagestate").innerHTML = document.readyState;;
+}
