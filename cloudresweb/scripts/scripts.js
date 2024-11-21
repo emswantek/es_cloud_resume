5707cb05e9d037d395bb2b915caa57b09ecb021e
Eric Swantek <162048271+emswantek@users.noreply.github.com>
2024-11-21 14:12:04 -0500

    changed response to jsonresponse

--- cloudresweb/scripts/scripts.js
+++ cloudresweb/scripts/scripts.js
@@ -7,13 +7,13 @@ async function fetchcount() {
   try {
     var vcount = document.getElementById('vcount');
     var jsondata
-    var response = await fetch("https://eswebfunctions01.azurewebsites.net/api/http_trigger3?");
+    var jsonresponse = await fetch("https://eswebfunctions01.azurewebsites.net/api/http_trigger3?");
 
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     }
 
-    jsondata = JSON.parse(response);
+    jsondata = JSON.parse(jsonresponse);
 
     vcount.innerHTML = jsondata.count
   } catch (error) {
