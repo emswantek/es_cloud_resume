706c2bc7737a96ff45bd26ffaf2c8839f4d88900
Eric Swantek <162048271+emswantek@users.noreply.github.com>
2024-11-21 14:33:56 -0500

    response body added

--- cloudresweb/scripts/scripts.js
+++ cloudresweb/scripts/scripts.js
@@ -7,13 +7,13 @@ async function fetchcount() {
   try {
     var vcount = document.getElementById('vcount');
     var jsondata
-    var jsonresponse = await fetch("https://eswebfunctions01.azurewebsites.net/api/http_trigger3?");
+    var response = await fetch("https://eswebfunctions01.azurewebsites.net/api/http_trigger3?");
 
     if (!response.ok) {
       throw new Error(`Error! status: ${response.status}`);
     }
 
-    jsondata = JSON.parse(jsonresponse);
+    jsondata = JSON.parse(response.body);
 
     vcount.innerHTML = jsondata.count
   } catch (error) {
