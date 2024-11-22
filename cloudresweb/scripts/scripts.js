2a9ecd2370fffda0cc578ac8ac03abf66184f284
ItsASine <git@itsasine.dev>
2024-11-21 20:46:33 -0500

    Handle large visitor counts

--- cloudresweb/scripts/scripts.js
+++ cloudresweb/scripts/scripts.js
@@ -12,13 +12,12 @@ async function getVisitorCount() {
     try {
         const visitorSpinner = document.getElementById('visitorSpinner')
         const visitorBadge = document.getElementById('visitorBadge')
-        const visitorCount = document.getElementById('visitorCount')
         const response = await fetch("https://eswebfunctions01.azurewebsites.net/api/http_trigger3?")
 
         if (response.ok) {
             const jsonData = await response.json()
 
-            visitorCount.innerHTML = jsonData.count
+            setVisitorCount(jsonData.count)
             visitorSpinner.style.display = 'none'
             visitorBadge.style.display = 'flex'
         } else {
@@ -30,3 +29,17 @@ async function getVisitorCount() {
     }
 }
 
+function setVisitorCount(count) {
+    const visitorCount = document.getElementById('visitorCount');
+    visitorCount.textContent = count;
+
+    if (count < 1000) {
+        visitorCount.style.fontSize = "16px";
+    } else if (count < 10000) {
+        visitorCount.style.fontSize = "12px";
+    } else if (count < 100000) {
+        visitorCount.style.fontSize = "8px";
+    } else {
+        visitorCount.style.fontSize = "4px"; // pls don't
+    }
+}
